51ab14f73edc9e964e5089d15cda5772b3aa6915
chhong <pnci1029@gmail.com>
2024-06-19 20:18:06 +0900

    add jsonstringify v2

--- src/main/kotlin/JsonStringify.kt
+++ src/main/kotlin/JsonStringify.kt
@@ -32,6 +32,30 @@ class JsonStringify {
             }
         builder.append('}')
         return "$builder"
+    }
 
+    fun <T : Any> stringifyV2(target: T): String {
+        val builder = StringBuilder()
+        builder.append('{')
+        /**
+         * KClass 코틀린 클래스 정의
+         * 클래스::class / 인스턴스::class
+         */
+        target::class.members
+            //Kproperty의 속성만 필터하는 함수. 이렇게 사용 안하면 두번 필터링해야함
+            .filterIsInstance<KProperty<*>>()
+            .joinTo(builder,",","{","}"){
+                val value = it.getter.call(target)
+                "${jsonString(it.name)}:${if(value is String) jsonString(value) else value}"
+
+            }
+        builder.append('}')
+        return "$builder"
+    }
+
+    class Json0(val a:Int, val b:String)
+
+    fun main(args:Array<String>){
+        println(stringifyV1(Json0(11,"bdbdf")))
     }
 }
