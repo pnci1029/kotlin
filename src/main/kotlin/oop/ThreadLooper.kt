c2a0b60e98da4731900cbf7acf18ff00e6d3dd0a
chhong <pnci1029@gmail.com>
2024-06-25 20:15:54 +0900

    add oop - thread looper

---
+++ src/main/kotlin/oop/ThreadLooper.kt
@@ -0,0 +1,36 @@
+package oop
+
+import java.time.Clock
+
+object ThreadLooper:Looper {
+    /**
+     * lazy -> 지연생성델리게이트
+     * 중괄호에 포함된 내용을 thread라는 속성이 사용되기전까지
+     * 사용되지 않음
+     */
+    private val thread by lazy{
+        Thread{
+            // 안전한 스레드 종료
+            while(isRunning && !Thread.currentThread().isInterrupted){
+                val now = Clock.systemDefaultZone()
+
+                // send 하는건 user에게 책임 전가
+                Looper.users.forEach { it.send(now) }
+                Thread.sleep(1000)
+            }
+        }
+    }
+
+    var isRunning = false
+        private set
+
+    override fun start() {
+        TODO("Not yet implemented")
+    }
+
+    override fun end() {
+        TODO("Not yet implemented")
+    }
+
+
+}
