a7b106fa7ec1ad2380a34763acb297104d3753fc
chhong <pnci1029@gmail.com>
2024-07-02 19:46:54 +0900

    루퍼 인터페이스 -> 추상클래스

--- src/main/kotlin/oop/Looper.kt
+++ src/main/kotlin/oop/Looper.kt
@@ -1,11 +1,30 @@
 package oop
+
+import oop.ThreadLooper.isRunning
+
 /**
  * 1. loopper 는 돌면서 user를 체크한다
  */
-interface Looper {
+abstract class Looper {
     companion object{
         var users = hashSetOf<User>()
     }
-    fun start()
-    fun end()
+
+
+    /**
+     속성은 없고 기능만 있으면 -> 인터페이스
+     추상 레이어에서 처리해야할 부분이 속성을 가지고 있으면 -> 추상클래스
+     */
+    var isRunning=false
+    fun start(){
+        isRunning = true
+        started()
+    }
+    fun end(){
+        isRunning = false
+        ended()
+    }
+
+    protected abstract fun started()
+    protected abstract fun ended()
 }
