8fa2e9f0d34260d6f8901266ae1bf30611ce69e1
chhong <pnci1029@gmail.com>
2024-07-02 20:05:12 +0900

    user 함수 추가

--- src/main/kotlin/oop/User.kt
+++ src/main/kotlin/oop/User.kt
@@ -1,4 +1,20 @@
 package oop
 
-class User {
+import java.time.Instant
+
+class User(
+    private val name:String
+) {
+    init {
+        Looper.users.add(this)
+    }
+    private val items = hashSetOf<Item>()
+    fun addItem(vararg item:Item){
+        items += item
+    }
+
+    fun send(now:Instant){
+        // 이 send 는 또 item으로 책임 전가
+        items.forEach{item -> item.send(now)}
+    }
 }
